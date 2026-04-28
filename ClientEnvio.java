a7b75a6601d33fe27cbd11db33752f0416a38d38
KamileKortz <kortzkamile@gmail.com>
2026-04-28 09:55:33 -0300

    Trabalho T1 - Mensagens em grupo

--- ClientEnvio.java
+++ ClientEnvio.java
@@ -1,20 +1,20 @@
-import java.io.*; // Importa classes do pacote java.io.*, serve pra entrada e saída de daods (InputStream, OutputStream)
-import java.net.*; // Importa classes do pacote java.net, serve pra comunicação em rede (Sockets)
-import java.util.Scanner; // Importa a classe Scanner, que serve pra ler dados digitados
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class ClientEnvio {
     public static void main(String[] args) throws Exception {
-        Socket s = new Socket("127.0.0.1", 9002); // Cria um socket pra se conectar com um servidor
-        OutputStream out = s.getOutputStream(); // Serve pra enviar dados para o servidor
+        Socket s = new Socket("127.0.0.1", 9002);
+        OutputStream out = s.getOutputStream();
         Scanner LER = new Scanner(System.in);
 
         // Lê o nome do usuário e envia pro servidor
         System.out.print("Nome: ");
         String nome = LER.nextLine();
-        out.write(nome.getBytes()); // Envia o nome para o servidor (nome.getBytes() trasforma o nome em Bytes, out.write envia os Bytes pela rede)
+        out.write(nome.getBytes());
 
         // Envia o que o usuário digita pro servidor
-        while (true) { // Esse while serve pro cliente poder ficar mandando várias mensagens pro servidor
+        while (true) { 
             String msg = LER.nextLine();
             out.write(msg.getBytes());
         }
